13fcac08c7c0d6a677df3c4bff53d40d51942ed0
VarSiv <var.mironov@gmail.com>
2025-03-15 00:02:32 -0300

    svelte config change

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+import path from 'path';
+
+
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  kit: {
+    // Specify the base path for GitHub Pages (repository name in the URL)
+    paths: {
+      base: '/visualizacion1', // Replace 'repository-name' with your GitHub repo name
+    },
+    // Other configurations here...
+  },
   preprocess: vitePreprocess(),
 }
+
