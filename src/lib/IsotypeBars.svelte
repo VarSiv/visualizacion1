19a3f535eff409cf501f2f69bd285ebdffa614da
VarSiv <var.mironov@gmail.com>
2025-03-23 11:48:15 -0300

    bars of desktop pc

--- src/lib/IsotypeBars.svelte
+++ src/lib/IsotypeBars.svelte
@@ -1,33 +1,57 @@
 <script>
-    import * as d3 from "d3"
-    export let title = ""
-    export let numbers = []
-    let numbersDivided5 = numbers.map(n => Math.floor(n / 5))
-  </script>
+  import * as d3 from "d3"
+  export let title = ""
+  export let numbers = []
   
-  <h3 class="headline">{title}</h3>
-  <div class="iso__container-iso">
-    {#each numbersDivided5 as n, index}
+  // Sort numbers from big to small
+  numbers.sort((a, b) => b - a)
+
+  let numbersDivided10 = numbers.map(n => Math.floor(n / 10))
+</script>
+
+<h3 class="headline">{title}</h3>
+<div class="iso__container-iso">
+  {#each numbersDivided10 as n, index}
       <div class="row">
-        {#each Array(n) as m}
-          <img
-            style="height: 50px; padding: 2px"
-            src="./images/person.png"
-            alt="person"
-          />
-        {/each}
-        <p>{numbers[index]}</p>
+          <p class="number-label">{numbers[index]}</p>
+          <div class="images-container">
+              {#each Array(n) as m}
+                  <img
+                      style="height: 70px; padding: 10px"
+                      src="./images/desktop.svg"
+                      alt="desktop computer"
+                  />
+              {/each}
+          </div>
       </div>
-    {/each}
-  </div>
-  
-  <style>
-    .iso__container-iso {
+  {/each}
+</div>
+
+<style>
+  .iso__container-iso {
       display: flex;
       flex-direction: column;
       max-width: 1000px;
       align-items: start;
       justify-content: space-around;
-    }
-  </style>
-  
+  }
+
+  .row {
+      display: flex;
+      align-items: center;
+      gap: 10px; 
+      margin-bottom: 15px;
+  }
+
+  .number-label {
+      font-weight: bold;
+      font-size: 18px;
+      text-align: left;
+      margin: 0;
+      padding: 0;
+  }
+
+  .images-container {
+      display: flex;
+  }
+</style>
